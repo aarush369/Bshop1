e35c500ec5f1e47c59c074fc6be1a86e2a5336eb
AARUSH <Aarush9709600280@gmail.com>
2023-05-07 18:28:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bshop1/Utils/FirebaseUtils.java
@@ -0,0 +1,28 @@
+package com.example.bshop1.Utils;
+
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirebaseUtils {
+
+    public  static String currentUserId(){
+
+        return FirebaseAuth.getInstance().getUid();
+
+    }
+
+
+
+
+
+    public static DocumentReference currentUserDetails(){
+
+        return FirebaseFirestore.getInstance().collection("user").document(currentUserId());
+
+    }
+
+
+}
